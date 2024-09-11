36afd171653b2417392cc37aa06178e7b81cf2ff
Luca Visentin <luca.visentin@unito.it>
2024-09-11 16:12:57 +0200

    Added package stuff around functions

--- R/denumerator.R
+++ R/denumerator.R
@@ -1,8 +1,24 @@
-requireNamespace("ggplot2")
-requireNamespace("assertthat")
-requireNamespace("reshape2")
-requireNamespace("patchwork")
-
+#' Convert a DESeq2 "result" object to an enumeration object
+#' 
+#' @param data A DESeq2 object from the 'results' function
+#' @param alpha The alpha p-value threshold above which a gene is considered
+#'    to be not differentially expressed. E.g. 0.05 would cause any gene with a
+#'    p-value of 0.06 to be not differentially expressed.
+#' @param fold_change The absolute fold change value a gene must have to be
+#'    considered to as differentially expressed. E.g. 2 would consider a gene
+#'    with fold change 1 to be not deferentially expressed.
+#' @param zero_lab Label to give in the enumeration to non-differentially expressed genes.
+#' @param pos_lab Label to give in the enumeration to upregulated genes.
+#' @param neg_lab Label to give in the enumeration to downregulated expressed genes.
+#' @param colname The column name to give to the variable with the labels in the resulting frame
+#' 
+#' @return A data.frame with one row per input gene and a column with the proper
+#'    enumeration labels based on the filter in the column "colname", by default
+#'    "class".
+#' 
+#' @author 
+#'    
+#' @export
 to_enumeration_vector <- function(
         data, alpha = 0.01, fold_change = 1,
         zero_lab = "zero", pos_lab = "positive", neg_lab = "negative",
@@ -28,7 +44,19 @@ to_enumeration_vector <- function(
     return(new_data)
 }
 
-apply_to_results <- function(deseq_object, results, fun, ..., pass_names = FALSE) {
+#' Apply `fun` to all results out of a deseq object.
+#' 
+#' @param deseq_object A Deseq Dataset with results (after the DESeq call)
+#' @param results A list of result names (as from `DESeq2::listNames`), for each
+#'    result on which the function should be applied to.
+#' @param fun A function to be applied
+#' @param ... Further arguments to be passed to `fun`
+#' 
+#' @returns A list with one frame per item in `results`, with the output of calling
+#'    `fun` on that result.
+#'    
+#' @export
+apply_to_results <- function(deseq_object, results, fun, ...) {
     res <- list()
     for (result in results) {
         print(result)
@@ -38,6 +66,39 @@ apply_to_results <- function(deseq_object, results, fun, ..., pass_names = FALSE
     res
 }
 
+
+#' Plot an enumeration frame from a list of enumeration data
+#' 
+#' This function takes care of computing frequency tables from input enumerations
+#' and plotting the resulting frequency graph.
+#' 
+#' @param enum_data A list with one slot per enumeration, with items returned by
+#'    `to_enumeration_vector`.
+#' @param order_by Order of the rows in the resulting plot. Either "frequency", to
+#'    sort by frequency, or "label" to sort by categorical labels (up, zero, down).
+#' @param zero_lab Label in the enumeration for non-differentially expressed genes.
+#' @param pos_lab Label in the enumeration for upregulated genes.
+#' @param neg_lab Label in the enumeration for downregulated expressed genes.
+#' @param title Title of the plot.
+#' @param top_n If not NULL, an integer. Keep only `top_n` rows by frequency.
+#' 
+#' @param positive_symbol The symbol in the plot to give to the upregulated case.
+#' @param negative_symbol The symbol in the plot to give to the negative case.
+#' @param positive_color The color in the plot to give to the upregulated case.
+#' @param negative_color The color in the plot to give to the negative case.
+#' @param symbol_size The size of the positive and negative legend symbols.
+#' @param symbol_fontface The font face of the positive and negative symbols.
+#' 
+#' @param labels_y_nudge By default, the symbols are centered on the correct
+#'    position. However, based on the used font, the symbols might be misaligned
+#'    in the Y axis. The "nudge" to give to each symbol is plot size and
+#'    font dependent, so it's difficult to compute beforehand.
+#'    Tweak this parameter to move the symbols up (e.g. +0.02) or down (e.g. -0.02)
+#'    so that they are perfectly aligned for your specific usecase.
+#' 
+#' @return A `ggproto` object with the resulting plot.
+#' 
+#' @export
 plot_enumeration_frame <- function(
         enum_data, order_by = c("frequency", "label"),
         zero_lab = "zero", pos_lab = "positive", neg_lab = "negative",
@@ -53,8 +114,8 @@ plot_enumeration_frame <- function(
     ord_factor_levels <- enum_data[[1]] %>% levels()
     
     # create the frequency matrix and recast it as d-frame
-    frequencies <- enum_data %>% as.data.frame() %>% ftable() %>% as.data.frame()
-    all_factors <- head(colnames(frequencies), -1)
+    frequencies <- enum_data %>% as.data.frame() %>% stats::ftable() %>% as.data.frame()
+    all_factors <- utils::head(colnames(frequencies), -1)
     
     # return to ordered factors
     for (lab in all_factors) {
@@ -72,7 +133,7 @@ plot_enumeration_frame <- function(
     # If we just want to show the top N, we dramatically subset here.
     # Drumroll...
     if (!is.null(top_n)) {
-        frequencies <- frequencies %>% slice_max(Freq, n = top_n)
+        frequencies <- frequencies %>% dplyr::slice_max(Freq, n = top_n)
         print(frequencies)
     }
     
@@ -86,24 +147,24 @@ plot_enumeration_frame <- function(
         stop(paste0("Invalid argument 'order': '", order_by, "'. Possibilities: 'frequency', 'label'"))
     }
     
-    bar_plot <- frequencies %>% ggplot(aes(x = Freq, y = label)) +
-        geom_bar(stat = "identity") +
-        ylab("") + xlab("Gene Count") +
-        scale_y_discrete(
+    bar_plot <- frequencies %>% ggplot2::ggplot(ggplot2::aes(x = Freq, y = label)) +
+        ggplot2::geom_bar(stat = "identity") +
+        ggplot2::ylab("") + ggplot2::xlab("Gene Count") +
+        ggplot2::scale_y_discrete(
             limits = frequencies$label[frequencies$order],
             labels=NULL
         ) +
-        theme_bw() +
-        geom_text(stat="identity", aes(label = Freq), hjust = -0.1) +
-        coord_cartesian(xlim = c(0, max(frequencies$Freq) * 1.10))+
-        theme_minimal() +
-        theme(
-            plot.margin = margin(0, 0, 0, 0, "pt"),
-            axis.text.y = element_blank(),
-            axis.ticks.y = element_blank(),
-            axis.line.y = element_blank(),
-            axis.title.y = element_blank(),
-            axis.ticks.length.y = unit(0, "pt")
+        ggplot2::theme_bw() +
+        ggplot2::geom_text(stat="identity", ggplot2::aes(label = Freq), hjust = -0.1) +
+        ggplot2::coord_cartesian(xlim = c(0, max(frequencies$Freq) * 1.10))+
+        ggplot2::theme_minimal() +
+        ggplot2::theme(
+            plot.margin = ggplot2::margin(0, 0, 0, 0, "pt"),
+            axis.text.y = ggplot2::element_blank(),
+            axis.ticks.y = ggplot2::element_blank(),
+            axis.line.y = ggplot2::element_blank(),
+            axis.title.y = ggplot2::element_blank(),
+            axis.ticks.length.y = ggplot2::unit(0, "pt")
         )
     
     melted_legend <- reshape2::melt(
@@ -118,31 +179,38 @@ plot_enumeration_frame <- function(
     melted_legend$neg <- ""
     melted_legend$neg[melted_legend$value == neg_lab] <- negative_symbol
     
-    label_plot <- melted_legend %>% ggplot(aes(x = variable, y = label)) +
-        geom_text(
-            aes(label=pos),
+    ## This is to appease the NOTEs section of devtools::check()
+    # See https://stackoverflow.com/questions/9439256/how-can-i-handle-r-cmd-check-no-visible-binding-for-global-variable-notes-when
+    # Fuck you CRAN, and fuck you R.
+    
+    Freq <- label <- margin <- neg <- pos <- variable <- NULL
+    
+    label_plot <- melted_legend %>% ggplot2::ggplot(ggplot2::aes(x = variable, y = label)) +
+        ggplot2::geom_text(
+            ggplot2::aes(label=pos),
             size = symbol_size, colour = negative_color, fontface=symbol_fontface,
             vjust=0.5, hjust=0.5,
             nudge_y=labels_y_nudge
         ) +
-        geom_text(
-            aes(label=neg),
+        ggplot2::geom_text(
+            ggplot2::aes(label=neg),
             size = symbol_size, colour = positive_color, fontface=symbol_fontface,
             vjust=0.5, hjust=0.5,
             nudge_y=labels_y_nudge
         ) +
-        scale_y_discrete(
+        ggplot2::scale_y_discrete(
             limits=melted_legend$label[frequencies$order],
             labels=NULL
         ) +
-        ylab(NULL) +
-        xlab(NULL) +
-        theme_minimal()+
-        theme(
-            plot.margin = margin(0, 0, 0, 0, "pt"),
-            axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)
+        ggplot2::ylab(NULL) +
+        ggplot2::xlab(NULL) +
+        ggplot2::theme_minimal()+
+        ggplot2::theme(
+            plot.margin = ggplot2::margin(0, 0, 0, 0, "pt"),
+            axis.text.x = ggplot2::element_text(angle = 90, vjust = 0.5, hjust=1)
         )
     
+    # We have the plots, we just have to tape them together.
     label_plot + patchwork::plot_spacer() + bar_plot +
         patchwork::plot_layout(
             ncol=3, nrow=1, widths=c(0.2, -0.04, 0.9),
@@ -153,10 +221,30 @@ plot_enumeration_frame <- function(
         )
 }
 
+
+#' D-enumerate a DESeq2 Dataset object with included results to an enumerator
+#' 
+#' With this function you can quickly move from a DESeq2 dataset with
+#' computed results (i.e. after calling `DESeq()`) and a complicated
+#' experimental design to something simpler that can be better observed
+#' with functions such as `plot_enumeration_frame`
+#' 
+#' @param computed_deseq_object DESeq2 Dataset object with included results
+#' @param results Either a vector of named results (as in from `DESeq2::resultsNames`)
+#'    or the special cases "nointercept" for all results without the intercept
+#'    and "all" for all results from the object.
+#' @param new_labels A list of `(old_label = new_label)` to replace the often
+#'    lengthy result labels in the DESeq2 object with more understandable ones.
+#'    The old labels are the same ones as specified in the `results` parameter.
+#' @param ... Further arguments to be passed to `to_enumeration_vector`.
+#'    Specifically, consider tweaking `alpha` and `fold_change`.
+#' 
+#' @returns A list of with one slot per result, with the computed enumerations.
+#' 
+#' @export
 denumerate <- function(
         computed_deseq_object, results = "nointercept",
-        new_labels = NULL,
-        ...
+        new_labels = NULL, ...
 ) {
     # Parse the "results" parameter
     possible_results <- DESeq2::resultsNames(computed_deseq_object)
@@ -176,7 +264,7 @@ denumerate <- function(
         computed_deseq_object,
         results,
         to_enumeration_vector,
-        pass_names = TRUE
+        ...
     )
     # I merge just with cbind(), so I want to be extra sure the rows are all
     # in the same order
@@ -188,7 +276,7 @@ denumerate <- function(
     enum_frame <- Reduce(cbind, enumerations)
     
     if (!is.null(new_labels)) {
-        enum_frame <- as.tibble(enum_frame) %>% rename_with(\(x) {
+        enum_frame <- tibble::as_tibble(enum_frame) %>% dplyr::rename_with(\(x) {
             .fn <- \(x) {
                 if (x %in% names(new_labels)) {
                     return(new_labels[[x]])
@@ -200,5 +288,5 @@ denumerate <- function(
         }) %>% as.data.frame()
     }
     
-    plot_enumeration_frame(enum_frame, ...)
+    enum_frame
 }
